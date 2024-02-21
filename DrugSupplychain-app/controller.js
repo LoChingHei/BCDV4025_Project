897b5661b6b57d00fc37903919160e0b55c4731d
LoChingHei <145512379+LoChingHei@users.noreply.github.com>
2024-02-21 18:12:08 -0500

    Update controller.js

--- DrugSupplychain-app/controller.js
+++ DrugSupplychain-app/controller.js
@@ -279,7 +279,6 @@ return{
 		console.log('Store path:'+store_path);
 		var tx_id = null;
 
-		// create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
 		Fabric_Client.newDefaultKeyValueStore({ path: store_path
 		}).then((state_store) => {
 		    fabric_client.setStateStore(state_store);
@@ -288,7 +287,6 @@ return{
 		    crypto_suite.setCryptoKeyStore(crypto_store);
 		    fabric_client.setCryptoSuite(crypto_suite);
 
-		    // get the enrolled user from persistence, this user will sign all requests
 		    return fabric_client.getUserContext('user1', true);
 		}).then((user_from_store) => {
 		    if (user_from_store && user_from_store.isEnrolled()) {
@@ -298,7 +296,6 @@ return{
 		        throw new Error('Failed to get user1.... run registerUser.js');
 		    }
 
-		    // get a transaction id object based on the current user assigned to fabric client
 		    tx_id = fabric_client.newTransactionID();
 		    console.log("Assigning transaction_id: ", tx_id._transaction_id);
 
